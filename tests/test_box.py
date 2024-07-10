4e3c1e0116db4f787e9f0e9ed84f12360fc5be32
AdrienSpecht <AdrienSpecht@users.noreply.github.com>
2024-07-10 14:01:14 -0700

    add methods to box manager

--- tests/test_box.py
+++ tests/test_box.py
@@ -1,15 +1,16 @@
 """Module that test if Box Manager works correctly"""
 
+import io
 import warnings
 from pathlib import Path
 
 import pandas as pd
 import pytest
 
-from box.manager import BoxManager
+from box.manager import PROTECTED_FOLDERS, BoxManager
 
 
-def test_box_manager():
+def test_root_folder():
     """Test access to Box proteomics folder"""
 
     with warnings.catch_warnings():
@@ -18,10 +19,9 @@ def test_box_manager():
         if not box.token_path.exists():
             pytest.skip("Box token file is not accessible, skipping box tests.")
 
-        # Test the content of the proteomics folder
-        content = set()
-        for item in box.proteomics.get_items():
-            content.add(item.name)
+        # Check the root folder
+        assert box.proteomics.name == "proteomics"
+        content = set(box.get_folder_contents(Path(".")))
         true_content = {
             "archives",
             "doc",
@@ -32,27 +32,102 @@ def test_box_manager():
             "READ_ME.txt",
         }
         assert content == true_content
-        assert box.proteomics.name == "proteomics"
 
-        # Test a folder id
+
+def test_get_id():
+    """Test get_id method"""
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", DeprecationWarning)
+        box = BoxManager()
+        if not box.token_path.exists():
+            pytest.skip()
+
+        # Check root id
+        path = Path(".")
+        root_id = box.get_id(path, box.proteomics)
+        assert root_id == "200099021915"
+        # Check folder id
         path = Path("raw_data")
         folder_id = box.get_id(path, box.proteomics)
         assert folder_id == "200093011900"
-        # Test a file id
+        # Check file id
         path = Path("raw_data/proteins/READ_ME.txt")
         file_id = box.get_id(path, box.proteomics)
         assert file_id == "1171334247062"
 
-        # Test a file.txt
-        path = Path("raw_data/proteins/READ_ME.txt")
+
+def test_txt_file():
+    """Test txt file upload/download/delete"""
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", DeprecationWarning)
+        box = BoxManager()
+        if not box.token_path.exists():
+            pytest.skip()
+        file = io.StringIO("Hello, world!")
+        path = Path("test/folder/hello.txt")
+
+        # Deletion of non-existent path
+        with pytest.raises(FileNotFoundError):
+            box.delete(path)
+        with pytest.raises(FileNotFoundError):
+            box.delete(path.parent)
+
+        # Create a folder with a file
+        with pytest.raises(ValueError):
+            box.create_folder(path)
+
+        # Create a new folder
+        box.create_folder(path.parent)
+        with pytest.raises(FileNotFoundError):
+            box.get_file(path)
+
+        # Create a folder that already exists
+        with pytest.raises(ValueError):
+            box.create_folder(path.parent)
+
+        # Upload the file
+        box.save_file(file, path)
+
+        # Upload a folder
+        with pytest.raises(ValueError):
+            box.save_file(file, path.parent)
+
+        # Download the file
         file = box.get_file(path)
-        true_beginning = "Last editor"
-        beginning = file.readline()[: len(true_beginning)]
-        assert beginning == true_beginning
-        # Test a file.csv
-        path = Path("archives/data/somasupp.csv")
+        assert file.readline() == "Hello, world!"
+
+        # Delete the file
+        box.delete(path)
+        with pytest.raises(FileNotFoundError):
+            box.get_file(path)
+
+        # Check protected folders
+        top_folder = Path(path.parts[0])
+        PROTECTED_FOLDERS.append(str(top_folder))
+        with pytest.raises(ValueError):
+            box.delete(top_folder)
+        PROTECTED_FOLDERS.remove(str(top_folder))
+
+        # Delete the top folder
+        box.delete(top_folder, recursive=True)
+        with pytest.raises(FileNotFoundError):
+            box.get_id(top_folder, box.proteomics)
+
+
+def test_csv_file():
+    """Test csv file upload/download/delete"""
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", DeprecationWarning)
+        box = BoxManager()
+        if not box.token_path.exists():
+            pytest.skip()
+        df_upload = pd.DataFrame({"A": [1, 2], "B": [4, None], "C": ["a", "b"]})
+        file = io.StringIO()
+        df_upload.to_csv(file, index=False)
+        file.seek(0)
+        path = Path("test.csv")
+        box.save_file(file, path)
         file = box.get_file(path)
-        df = pd.read_csv(file)
-        assert df.shape == (7289, 11)
-        subset_cols = {"SeqId", "Target Name", "UniProt ID"}
-        assert subset_cols.issubset(df.columns)
+        df_download = pd.read_csv(file)
+        assert df_upload.equals(df_download)
+        box.delete(path)
